1fb036c3bff63f6270bd564c8924a94192dddc4a
syx <791649526@qq.com>
2020-11-22 16:57:43 +0800

    v12 全部功能完成

--- article/publisher/publisher.js
+++ article/publisher/publisher.js
@@ -5,7 +5,7 @@ $.ajax({
   success: function (res) {
 
     if (res.status == 0) {
-      var str = "";
+      var str = `<option value="">所有分类</option>`;
       $.each(res.data, function (index, ele) {
         str += `<option value="${ele.Id}">${ele.name}</option>`;
       });
@@ -97,7 +97,7 @@ $("form").on("submit", function (e) {
         // 设计：
         if (res.status == 0) {
           // 页面转跳到 文章列表 页面
-          location.href = '/article/article/article.html';
+          location.href = '/article/list/list.html';
 
           // 响应dom节点 类名添加
           var dom = window.parent.document.querySelector("#wzlist");
